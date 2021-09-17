2ec1f86ccd7a08eef92f33c5d122e1c8f93d84c3
Charles Adams <87811346+etotheipiequals@users.noreply.github.com>
2021-09-17 15:00:22 +0100

    Caroline Quiz

---
+++ Caroline Quiz/caroline/template_quiz/__main__.py
@@ -0,0 +1,35 @@
+import os
+from distutils.dir_util import copy_tree
+import shutil
+
+print("Writting example presentation in the given directory...")
+
+carolineHTML = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "html_dist"
+)
+
+carolineDestination = os.path.join(os.getcwd(), "caroline")
+if not os.path.exists(carolineDestination):
+    os.makedirs(carolineDestination)
+
+copy_tree(carolineHTML, carolineDestination)
+
+presentationFilesDestination = os.path.join(os.getcwd(), "presentation_files")
+presentationFiles = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "presentation_files"
+)
+copy_tree(presentationFiles, presentationFilesDestination)
+
+dataFolder = os.path.dirname(os.path.realpath(__file__))
+destinationFolder = os.getcwd()
+shutil.copy(os.path.join(dataFolder, "presentation_code.py"), destinationFolder)
+shutil.copy(os.path.join(dataFolder, "presentation.html"), destinationFolder)
+shutil.copy(os.path.join(dataFolder, "presentation_audience.html"), destinationFolder)
+shutil.copy(os.path.join(dataFolder, "simple_server.py"), destinationFolder)
+print(
+    "Presentation template created.\n\nTo edit it change presentation_code.py and run:"
+)
+print("    python presentation_code.py")
+print(
+    "\nTo see it run \n   python simple_server.py\n or refresh browser if you are already running simple_server.py"
+)
