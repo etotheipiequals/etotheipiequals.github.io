54a3e95b744c2a0463926356ba09e3e1ccf79555
Charles Adams <87811346+etotheipiequals@users.noreply.github.com>
2021-10-08 07:10:41 +0100

    QIC_Caroline update 2021_10_08

--- QIC_Caroline/QIC_1.py
+++ QIC_Caroline/QIC_1.py
@@ -2,16 +2,71 @@ from caroline import Presentation
 
 p = Presentation(
     logo="./presentation_files/Durham_University_Logo.png",
-    leftHanded=False,
+    leftHanded=True,
     drawingHelp="lines",
     drawingHelpIntensity=0.04,
-#    roundTableServer="https://roundtable.researchx3d.com",
-#    presentationServer="https://etotheipiequals.github.io/Caroline_Quiz/demo_audience.html",
-#    authenticationToken="osagASfew8t31qNqfHQ3Gq",
+    roundTableServer="https://roundtable.researchx3d.com",
+    presentationServer="https://etotheipiequals.github.io/QIC_Caroline/QIC_1_audience.html",
+    authenticationToken="osagASfew8t31qNqfHQ3Gq",
 )
 
 # drawingHelp = ""  OR "lines"  OR  "dots"
 # drawingHelpIntenisty = flaot [0,1] ; 0=white   1 = black
+
+
+########################################
+############# TITLE slide ##############
+########################################
+
+p.newSlide()
+p.spanCenterText(r"""
+
+
+
+** C. S. Adams**
+
+**Quantum information and computing** 
+
+** QIC **
+
+""", fontSize = 1.0)
+
+p.leftText(r"""
+
+**Aims**
+
+* How to build a **quantum computer**?
+
+* Forefront of current knowledge (decoherence and entanglement) and technology.
+ 
+""")
+
+p.rightImage("./Figures/Ramsey_static.png", height = 400) ########################################
+#############  New slide  ############## What I do? Teaching 
+########################################
+
+p.newSlide()
+
+p.title("QIC.0 Outline")
+p.leftText(r"""
+
+
+** Resources (Ultra) **
+
+* Notes
+
+* Slides (with annotations) .pdf (without).html files
+
+** Outline **
+
+* Lecture 1--10: Theoretical framework
+
+* Lecture 11--18: Practical implementation (atoms, lasers and qubits)
+
+
+""")
+p.rightImage("./Figures/QIC_0_p_1.png", height = 400)
+
 
 ########################################
 ############# TITLE slide ##############
@@ -24,18 +79,131 @@ p.spanCenterText(r"""
 C. S. Adams,  Durham University, Jan 2021.
 """, fontSize = 1.0)
 
-
 p.leftText(r"""
-* Why quantum computing?
+
+Outline
 
 * What is a qubit?
+
+* Examples
+
+* Why quantum computing?
 
 * Exponential scaling.
  
 """)
 
+p.rightImage("./Figures/Ramsey_static.png", height = 400) 
 
-p.rightImage("./Figures/Ramsey_static.png", height = 600) 
+########################################
+#############  New slide  ############## What I do? Teaching 
+########################################
+
+p.newSlide()
+
+p.title("QIC.1 Notes")
+p.leftText(r"""
+* QIC.1 Notes 
+
+ * *Circuit Quantum Electrodynamics*, A Blais, AL Grimsmo, SM Girvin, A Wallraff, 
+https://arxiv.org/abs/2005.12667
+
+""", fontSize = 0.5)
+p.rightImage("./Figures/QIC_1_p_1.png", height = 400)
+
+
+##################################/######
+#############  New slide  ##############
+########################################
+
+p.newSlide()
+p.title("What is quantum computing? Bits vs. Qubits")
+
+
+p.makeGrid(4,4)
+p.gridText(0,2,r"""
+
+Classical bit 0 or 1
+
+Quantum bit 0 and 1 
+
+(but only if we do not look!)
+""", fontSize = 0.5)
+
+p.gridImage(0,3,"./Figures/Bit_vs_Qubit.JPG", height = 100)
+
+p.gridText(1,2,r"""
+
+** $\vert \psi\rangle = a \vert 0 \rangle + b \vert 1 \rangle$ **
+
+""", fontSize = 0.5)
+
+
+
+p.gridImage(1,3,"./QIC_Figures/QIC1_qubit.png", height = 100)
+
+
+p.gridImage(2,2,"./Figures/2s12m12.png", height = 100)
+
+p.gridImage(2,3,"./Figures/2p12m12.png", height = 100)
+
+
+
+
+#p.gridImage(2,3,"./Figures/Feynman_85.png", height = 100)
+
+
+########################################
+#############  New slide  ##############
+########################################
+
+p.newSlide()
+p.title("What is a qubit?")
+p.spanCenterText(r"""
+
+** $\vert \psi\rangle = a \vert 0 \rangle + b \vert 1 \rangle$ **
+
+""", fontSize = 2.0)
+
+p.spanCenterText(r"""
+
+*The wave function [or state vector] is NOT a description of the quantum object, it is a prescription for what to expect when we make a measurement on that object*. Phillip Ball, *Quantum mechanics is not weird*, Qiskit Seminar, 12th February 2021.
+
+""", fontSize = 0.5)
+
+
+########################################
+#############  New slide  ##############
+########################################
+
+p.newSlide()
+p.title("Qubit (two-level system): Examples ")
+
+p.makeGrid(4,4)
+
+p.gridImage(0,2,"./QIC_Figures/QIC1_superconducting_3.jpg", height = 100)
+p.gridImage(0,3,"./QIC_Figures/QIC1_superconducting_1.png", height = 100)
+
+p.gridImage(1,2,"./QIC_Figures/QIC1_ions_1.jpg", height = 100)
+p.gridImage(1,3,"./QIC_Figures/QIC1_ions_2.png", height = 100)
+
+p.gridImage(2,3,"./QIC_Figures/QIC11_Atom_QC_Review_2.png", height = 100)
+
+
+########################################
+#############  New slide  ##############
+########################################
+
+p.newSlide()
+p.title("Qubit (two-level system): Examples ")
+
+p.makeGrid(4,4)
+
+p.gridImage(0,2,"./QIC_Figures/QIC1_pol_left_right.png", height = 100)
+p.gridImage(0,3,"./QIC_Figures/QIC1_photons_1.png", height = 100)
+
+p.gridImage(1,3,"./QIC_Figures/QIC1_semi.png", height = 100)
+
 
 
 ########################################
@@ -45,13 +213,20 @@ p.rightImage("./Figures/Ramsey_static.png", height = 600)
 p.newSlide()
 p.title("Why quantum computing?")
 p.makeGrid(4,4)
-p.gridText(0,0,r"""
-""", fontSize = 1.0)
+p.gridText(0,1,r"""
+*no exponential is forever... But we can delay ``forever''*. 
 
+Gordon Moore
 
-p.gridImage(0,3,"./Figures/Moores_law_48_years.png", height = 100)
+Slide 29 in *No_Exponential_is_Forever* at the International Solid State Circuits Conference 50th anniversary, 2003.
+""", fontSize = 0.5)
+
+p.gridImage(0,2,"./Figures/Moores_law_48_years.png", height = 100)
+p.gridImage(0,3,"./QIC_Figures/QIC1_feature_size.png", height = 100)
+
 p.gridImage(1,3,"./Figures/Moores_law_energy.png", height = 100)
-p.gridImage(2,3,"./Figures/Feynman_85.png", height = 100)
+p.gridImage(2,2,"./Figures/Feynman_85.png", height = 100)
+p.gridImage(2,3,"./QIC_Figures/QIC1_energy_2.png", height = 100)
 
 
 ########################################
@@ -59,17 +234,75 @@ p.gridImage(2,3,"./Figures/Feynman_85.png", height = 100)
 ########################################
 
 p.newSlide()
-p.title("Motivation")
+p.title("Quantum advantage")
 
+
+########################################
+############# INTERACTIVE FIGURE  ##############
+########################################
+
+p.newSlide()
+p.title("Quantum advantage")
+
+
+########################################
+#############  New slide  ############## What I do? Teaching 
+########################################
+
+p.newSlide()
+
+p.title("QIC.1 Summary")
 p.leftText(r"""
-* Animations
- 
-""")
+** QIC.1 Notes ** 
 
-p.rightImage("./Figures/Moores_law_48_years.png", height=600)
+Summary 
 
 
+""", fontSize = 1.0)
+p.rightImage("./QIC_Figures/QIC1_summary.png", height = 400)
 
+
+########################################
+############# QUIZ  ##############
+########################################
+
+p.newQuiz(
+    questionImage="./Figures/Bohr.jpg",
+    questionText=r"""## Which of the following are superpositions of the form, $\vert \psi\rangle = \textstyle{1\over\sqrt{2}}\left( \vert 0 \rangle \pm  \vert 1 \rangle \right)$ ?""",
+    answersImage=[
+        "./Figures/2s12m12.png",
+        "./Figures/2p12m12.png",
+        "./Figures/1s12m12.png",
+        "./Figures/2s12m12+2p12m32.png",
+        "./Figures/2s12m12+2p12m12.png",
+        "./Figures/2p12m32.png",
+    ],
+)
+
+########################################
+############# QUIZ slide ##############
+########################################
+
+p.newQuiz(
+    questionText="#The amount of information that can be stored in one qubit is",
+    answersText=[r"One bit.",
+                 r"Two bits.",
+                 r"Infinite.",
+                 r"In depends on the measurement."]
+)
+
+
+########################################
+############# QUIZ slide ##############
+########################################
+
+p.newQuiz(
+    questionText="#The amount of information that can be retrieved from one qubit is",
+    answersText=[r"One bit.",
+                 r"Two bits.",
+                 r"Infinite.",
+                 r"In depends on the measurement."]
+)
 
 
 ########################################
